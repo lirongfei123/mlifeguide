b6f59603e169097fb55411b1841f043e9e1b56f0
rongping.lrp <rongping.lrp@alibaba-inc.com>
2017-04-09 12:16:06 +0800

    ceshi

--- src/app/home/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './home.component';
-import { ContentEditorComponent } from './home.editor.component';
-import { BacisEditorComponent } from './basic/basic.component';
-
-export const HomeChildCompoent = [
-    ContentEditorComponent,
-    BacisEditorComponent
-];
