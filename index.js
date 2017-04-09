7b92f14001a2edd89cd0d9468c210256219eefdc
rongping.lrp <rongping.lrp@alibaba-inc.com>
2017-04-09 12:25:29 +0800

    ceshi

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var url = require('url');
-var app = express();
-var setUp = require('./appset');
-setUp(app, 'prod');
-var proxy = require('express-http-proxy');
-app.use('/my-project/mlife-guide-re/', proxy('www.mlife.top', {
-    forwardPath: function(req, res) {
-        return '/my-project/mlife-guide-re/' + url.parse(req.url).path;
-    }
-}));
-app.listen(process.env.PORT || 5050);
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.PORT || 5050)
