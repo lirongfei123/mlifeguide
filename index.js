270797dcfc0d58cade127dbd8cdf0a3151e1a266
rongping.lrp <rongping.lrp@alibaba-inc.com>
2017-04-26 19:08:09 +0800

    还原

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(process.env.PORT || 5050)
+var express = require('express');
+var url = require('url');
+var app = express();
+var setUp = require('./appset');
+setUp(app, 'prod');
+var proxy = require('http-proxy-middleware');
+app.use('/my-project/mlife-guide-re/', proxy({
+    target: 'http://www.mlife.top',
+    changeOrigin: true, 
+    secure: true,
+    pathRewrite: {
+        '^/my-project/mlife-guide-re' : '/project/mlife-guide-re'
+    }
+}));
+app.listen(process.env.PORT || 5050);
